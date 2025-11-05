3acd458bc18bfebf791cab99fadcf8b136ac809a
cono200 <tatattian04@gmail.com>
2025-11-04 21:32:27 -0700

    feat(project): Esqueleto de la api archivo program y conexion

---
+++ Data/TeamFinderDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeamFinder.Api.Data
+{
+    public class TeamFinderDbContext : DbContext
+    {
+        public TeamFinderDbContext(DbContextOptions<TeamFinderDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets se agregarán aquí más adelante
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeamFinder.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// --- A�ADE ESTA L�NEA ---
+// Registra el DbContext para que la aplicaci�n sepa c�mo crearlo.
+// Le dice que use SQL Server y que la cadena de conexi�n est� en "DefaultConnection".
+builder.Services.AddDbContext<TeamFinderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
