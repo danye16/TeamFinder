63ecf49d8b3799466dcaafb09db756689520cb27
cono200 <tatattian04@gmail.com>
2025-12-01 09:53:27 -0700

    Archivo deploy puesto

--- obj/Debug/net9.0/TeamFinder.AssemblyInfo.cs
+++ obj/Debug/net9.0/TeamFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05b615b548014c05b326fa2421305f8befbc395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f123c1dc726837f32bd0f9dcebf076b7ddf9b16d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
